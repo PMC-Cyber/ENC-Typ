79c9ccf562e7de55ce30f94a73d853f1fcaf69a5
Zephyr UCH-2009 <139885500+Unlimited-Crack-You@users.noreply.github.com>
2023-10-13 08:57:50 +0700

    Update Install-script.sh

--- Install-script.sh
+++ Install-script.sh
@@ -1,53 +1,6 @@
 # athor : UCH-2009
 # Tools Version : 0.1
 # Admin website: www.uch-2009.com
-clear
-
-b='\033[1m'
-u='\033[4m'
-bl='\E[30m'
-r='\E[31m'
-g='\E[32m'
-bu='\E[34m'
-m='\E[35m'
-c='\E[36m'
-w='\E[37m'
-endc='\E[0m'
-enda='\033[0m'
-blue='\e[1;34m'
-cyan='\e[1;36m'
-red='\e[1;31m'
-
-RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
-MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
-REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
-MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
-DEFAULT_FG="$(printf '\033[39m')"  DEFAULT_BG="$(printf '\033[49m')"
-
-echo " "
-echo "${GREEN} ╦ ╔╗╔ ╔═╗ ╔╦╗ ╔═╗ ╦┈┈ ╦┈┈ ┈ ╔═╗ ╔═╗ ╦═╗ ╦ ╔═╗ ╔╦╗  "
-echo "${GREEN} ║ ║║║ ╚═╗ ┈║┈ ╠═╣ ║┈┈ ║┈┈ ┈ ╚═╗ ║┈┈ ╠╦╝ ║ ╠═╝ ┈║┈  "
-echo "${GREEN} ╩ ╝╚╝ ╚═╝ ┈╩┈ ╩┈╩ ╩═╝ ╩═╝ ┈ ╚═╝ ╚═╝ ╩╚═ ╩ ╩┈┈ ┈╩┈  "
-sleep 1s
-echo "${WHITE} Script By : UCH 2009 FROM"
-sleep 1s
-echo  " "
-echo "${BLUE}[${RED}!${BLUE}] ${GREEN}Tuan Mengunakan Software installasi Termux tunggu sampai instalasinya selesai"
-sleep 1s
-echo "${BLUE}[${RED}✓${BLUE}] ${GREEN}Loading Installing In Termux..."
-    echo " "
-      sleep 2s
-   echo "~{${ORANGE} Bersiap Untuk Menginstal Tuan ${GREEN}}~"
-      sleep 3s
-    echo " "
-pkg upgrade && pkg update
-pkg install git
-pkg install python2
-pkg install python
-pip2 install termcolor
-pkg install nodejs
-pkg install neofetch -y
-npm install -g bash-obfuscate
-bash ENC-TTp.sh
-
-# copyright © UCH-2009
+z="
+";iCz='═╝ ╩';ECz='echo';WCz=' ┈ ╚';aEz=' ENC';AEz='&& p';ZDz='adin';KCz=' ╔╦╗';iDz=' "~{';ZEz='bash';gCz=' ╩═╝';kDz='} Be';wBz='ULT_';XEz='obfu';TDz='lasi';qz='GREE';jDz='${OR';UDz='nya ';xCz='BLUE';KDz='re i';eBz='3[42';vDz='p 3s';BDz='!${B';Gz='4m'\''';tz='ntf ';ACz='"  D';RBz='intf';tDz='EEN}';tCz='009 ';aCz=' ╠═╝';uBz='40m'\''';bDz='stal';ABz='="$(';PDz=' tun';pBz='[46m';LDz='nsta';DBz='\033';cBz='  GR';Az='clea';rz='N="$';aDz='g In';hBz='3m'\'')';Uz='w='\''\';SCz=' ╚═╗';eCz=' ┈╩┈';mBz='3[45';EBz='[33m';TEz=' -y';nDz='tuk ';DDz='] ${';TCz=' ┈║┈';yBz='9m'\'')';yCz='}[${';ez='[1;3';OBz=' WHI';FCz=' " "';VCz=' ║┈┈';lBz='NTAB';rBz='[47m';WBz='ACK=';qCz='t By';az='='\''\0';nBz='  CY';QBz='$(pr';iz=''\''\e[';NDz='i Te';Tz='E[36';VBz='" BL';aBz='G="$';EDz='N}Tu';mz='rint';dCz=' ╚═╝';MEz=' ter';ADz='RED}';fDz='ux..';tBz='KBG=';CDz='LUE}';HEz='on2';sDz='${GR';Kz='r='\''\';wCz='  " ';DEz='e';PCz='  "';fz='cyan';OCz='═╗ ╦';dBz='EENB';gDz='."';XDz='✓${B';XCz='═╗ ║';bBz='3[41';QDz='ggu ';Pz='bu='\''';XBz='30m'\''';UCz=' ╠═╣';oCz='E} S';HBz='E="$';JCz=' ╔═╗';pz=')"  ';kz='RED=';KBz='NTA=';ODz='rmux';sz='(pri';LCz=' ╦┈┈';Mz='m'\''';MCz=' ┈ ╔';DCz='3[49';BEz='kg u';fBz='BG="';CCz='LT_B';PBz='TE="';ICz=' ╔╗╔';YDz='N}Lo';SDz='ai i';FBz=''\'')" ';Iz='\E[3';xBz='FG="';yz='ANGE';vz='3[32';NEz='mcol';hz='red=';FDz='an M';KEz=' ins';bCz='N} ╩';YBz=')"';Dz='033[';BCz='EFAU';lDz='rsia';lz='"$(p';QCz='N} ║';pDz='inst';Ez='1m'\''';RDz='samp';rCz=' : U';wDz='pkg ';xz='  OR';WEz='ash-';Sz='c='\''\';vCz='"';mDz='p Un';HDz='naka';kBz='3[44';GCz=' "${';FEz='git';uz=''\''\03';QEz='js';Oz='E[32';WDz='sai"';nz='f '\''\';cDz='ling';Cz='b='\''\';CEz='pdat';mCz='p 1s';JBz='MAGE';sBz='BLAC';OEz='or';MBz='CYAN';yDz='ade ';GEz='pyth';gBz='33[4';MDz='llas';Wz='endc';CBz='tf '\''';bz='33[0';oDz='Meng';gz='6m'\''';IEz='on';JDz='ftwa';dz='='\''\e';hDz='p 2s';JEz='pip2';fCz=' ╩┈╩';oz='31m'\''';LEz='tall';uCz='FROM';ZBz='REDB';Qz='m='\''\';GDz='engu';dDz=' In ';jz='1;31';PEz='node';UBz='7m'\'')';RCz=' ║║║';lCz='slee';Yz='[0m'\''';Fz='u='\''\';hCz='═╝ ╚';ZCz='╦╝ ║';Rz='E[35';SBz=' '\''\0';pCz='crip';Xz='='\''\E';IDz='n So';xDz='upgr';oBz='ANBG';kCz=' ╩┈┈';EEz='all ';Jz='0m'\''';rDz='uan ';LBz='35m'\''';Nz='g='\''\';NCz='═╗ ╔';uDz='}~"';VEz='-g b';eDz='Term';Vz='E[37';vBz='DEFA';cEz='.sh';sCz='CH 2';qBz='TEBG';YCz='┈┈ ╠';nCz='WHIT';cCz=' ╝╚╝';jBz='LUEB';cz='blue';Lz='E[31';TBz='33[3';IBz='3[34';Zz='enda';iBz='"  B';HCz='N} ╦';SEz='etch';jCz='╚═ ╩';Hz='bl='\''';YEz='scat';BBz='prin';qDz='al T';NBz='[36m';REz='neof';bEz='-TTp';UEz='npm ';wz='m'\'')"';Bz='r';VDz='sele';GBz=' BLU';
+eval "$Az$Bz$z$Cz$Dz$Ez$z$Fz$Dz$Gz$z$Hz$Iz$Jz$z$Kz$Lz$Mz$z$Nz$Oz$Mz$z$Pz$Iz$Gz$z$Qz$Rz$Mz$z$Sz$Tz$Mz$z$Uz$Vz$Mz$z$Wz$Xz$Yz$z$Zz$az$bz$Mz$z$cz$dz$ez$Gz$z$fz$dz$ez$gz$z$hz$iz$jz$Mz$z$kz$lz$mz$nz$Dz$oz$pz$qz$rz$sz$tz$uz$vz$wz$xz$yz$ABz$BBz$CBz$DBz$EBz$FBz$GBz$HBz$sz$tz$uz$IBz$wz$z$JBz$KBz$lz$mz$nz$Dz$LBz$pz$MBz$ABz$BBz$CBz$DBz$NBz$FBz$OBz$PBz$QBz$RBz$SBz$TBz$UBz$VBz$WBz$lz$mz$nz$Dz$XBz$YBz$z$ZBz$aBz$sz$tz$uz$bBz$wz$cBz$dBz$aBz$sz$tz$uz$eBz$wz$xz$yz$fBz$QBz$RBz$SBz$gBz$hBz$iBz$jBz$aBz$sz$tz$uz$kBz$wz$z$JBz$lBz$aBz$sz$tz$uz$mBz$wz$nBz$oBz$ABz$BBz$CBz$DBz$pBz$FBz$OBz$qBz$ABz$BBz$CBz$DBz$rBz$FBz$sBz$tBz$lz$mz$nz$Dz$uBz$YBz$z$vBz$wBz$xBz$QBz$RBz$SBz$TBz$yBz$ACz$BCz$CCz$aBz$sz$tz$uz$DCz$wz$z$ECz$FCz$z$ECz$GCz$qz$HCz$ICz$JCz$KCz$JCz$LCz$LCz$MCz$NCz$OCz$OCz$JCz$KCz$PCz$z$ECz$GCz$qz$QCz$RCz$SCz$TCz$UCz$VCz$VCz$WCz$XCz$YCz$ZCz$aCz$TCz$PCz$z$ECz$GCz$qz$bCz$cCz$dCz$eCz$fCz$gCz$gCz$WCz$hCz$iCz$jCz$kCz$eCz$PCz$z$lCz$mCz$z$ECz$GCz$nCz$oCz$pCz$qCz$rCz$sCz$tCz$uCz$vCz$z$lCz$mCz$z$ECz$wCz$vCz$z$ECz$GCz$xCz$yCz$ADz$BDz$CDz$DDz$qz$EDz$FDz$GDz$HDz$IDz$JDz$KDz$LDz$MDz$NDz$ODz$PDz$QDz$RDz$SDz$LDz$TDz$UDz$VDz$WDz$z$lCz$mCz$z$ECz$GCz$xCz$yCz$ADz$XDz$CDz$DDz$qz$YDz$ZDz$aDz$bDz$cDz$dDz$eDz$fDz$gDz$z$ECz$FCz$z$lCz$hDz$z$ECz$iDz$jDz$yz$kDz$lDz$mDz$nDz$oDz$pDz$qDz$rDz$sDz$tDz$uDz$z$lCz$vDz$z$ECz$FCz$z$wDz$xDz$yDz$AEz$BEz$CEz$DEz$z$wDz$pDz$EEz$FEz$z$wDz$pDz$EEz$GEz$HEz$z$wDz$pDz$EEz$GEz$IEz$z$JEz$KEz$LEz$MEz$NEz$OEz$z$wDz$pDz$EEz$PEz$QEz$z$wDz$pDz$EEz$REz$SEz$TEz$z$UEz$pDz$EEz$VEz$WEz$XEz$YEz$DEz$z$ZEz$aEz$bEz$cEz"
