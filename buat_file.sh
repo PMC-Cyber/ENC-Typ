0c165b76d1c799fc60db3d14197b57da0a303bfe
PMC MALWARE CYBER <139885500+Unlimited-Crack-You@users.noreply.github.com>
2023-12-11 12:24:03 +0700

    Update buat_file.sh

--- buat_file.sh
+++ buat_file.sh
@@ -1,7 +1,17 @@
-echo " NOTE :"
+## ANSI colors (FG & BG)
+R="$(printf '\033[31m')"  
+GR="$(printf '\033[32m')"  
+O="$(printf '\033[33m')"  
+BL="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"  
+CYAN="$(printf '\033[36m')"  
+WT="$(printf '\033[37m')" 
+BLK="$(printf '\033[30m')"
+clear
+echo $WT" NOTE :"
 sleep 1
-echo " SEBELUM MASUK KE TOOLS NYA ANDA BUAT FILE YANG MAU ENC ATAU DNC "
+echo $CYAN" SEBELUM MASUK KE TOOLS NYA ANDA BUAT FILE YANG MAU ENC ATAU DNC "
 sleep 1
-echo " CARA NYA KETIKAN ( nano coba.sh ) SESUDAH KETIKAN ITU TEMPEL FILE NYA "
+echo $CYAN" CARA NYA KETIKAN ${WT}( ${GR}nano coba.sh${WT} ) ${CYAN}SESUDAH KETIKAN ITU TEMPEL FILE NYA "
 sleep 1
-echo " SESUAI BUAT FILE NYA LALU KETIKAN LAGI BUAT RUN SC NYA KETIK bash index.sh "
+echo " SESUAI BUAT FILE NYA LALU KETIKAN LAGI BUAT RUN SC NYA KETIK${GR} bash index.sh "
